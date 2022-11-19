7412598b5f76cca02b2e7b5ae10313beba4fca6a
mastas21 <97802500+mastas21@users.noreply.github.com>
2022-11-18 20:47:56 -0500

    Add files via upload

--- q2.py
+++ q2.py
@@ -1,13 +1,15 @@
 
 
+import scipy
+import numpy as np 
 import pandas as pd
 import matplotlib.pylab as plt
-df = pd.read_excel("test.xlsx",index_col=0,
+
+df = pd.read_excel("test0.xlsx",index_col=0,
                    header=None)
-print(df[2])
+
 s=pd.to_numeric(df[1][2:])
 p=pd.to_numeric(df[2][2:])
 l=pd.to_numeric(df[3][2:])
-
-plt.scatter(p,l, color='coral')
-plt.savefig("NCLOC_DCP.jpg")
+r, p = scipy.stats.pearsonr(p, l)
+print(r)
